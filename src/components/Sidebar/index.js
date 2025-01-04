c88c4f04cb01f3021e8a58ddf5756082a30c883d
Yoyo <xche558@aucklanduni.ac.nz>
2025-01-05 04:38:48 +1300

    Implement Nav Bar
    Added in the nav bar, basic pages set up. Need to add in own logo and portfolio page later

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,50 @@
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+// TODO: Get and use own logo
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => (
+    <div className="nav-bar">
+        <Link className ='logo' to ='/'>
+            <img src={LogoS} alt='logo' />
+            <img classname='sub-logo' src={LogoSubtitle} alt='slobodan' />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#132735" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#132735" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#132735" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/yoyochen7/"
+                >
+                    <FontAwesomeIcon icon = {faLinkedin} color = "#132735" />
+                </a>
+            </li>
+            <li>
+                <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/AlotOfTypos"
+                >
+                    <FontAwesomeIcon icon = {faGithub} color = "#132735" />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
