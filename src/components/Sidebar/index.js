e0b44e954c43004af8968fe35296b26acb8c17b1
Yoyo <xche558@aucklanduni.ac.nz>
2025-01-25 20:16:58 +1300

    Logo
    add in logo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,8 +1,6 @@
 import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
-// TODO: Get and use own logo
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import Logo from '../../assets/images/catppacino_brown.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
@@ -10,8 +8,7 @@ import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const Sidebar = () => (
     <div className="nav-bar">
         <Link className ='logo' to ='/'>
-            <img src={LogoS} alt='logo' />
-            <img classname='sub-logo' src={LogoSubtitle} alt='slobodan' />
+            <img src={Logo} alt='logo' />
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
